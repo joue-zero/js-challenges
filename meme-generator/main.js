4ce71a9126e752ba1880077ffa8356e94a26143b
joue-zero <elnagaryoussef864@gmail.com>
2022-07-31 15:42:56 +0200

    Version One Of Meme Generator

---
+++ meme-generator/main.js
@@ -0,0 +1,34 @@
+
+let topI = document.querySelector('input.top'),
+    bottomI = document.querySelector('input.bottom'),
+    button = document.querySelector('button'),
+    img = document.querySelector('.meme-image');
+
+topI.addEventListener('keyup', (e) => {
+    putInput(e.composedPath()[0]);
+});
+
+
+bottomI.addEventListener('keyup', (e) => {
+    putInput(e.composedPath()[0]);
+});
+
+function putInput(e){
+    document.querySelector(`p.${e.getAttribute('class')}`).textContent = e.value;
+}
+
+// Fetch The Meme Api
+let arr = [];
+fetch('https://api.imgflip.com/get_memes').then((re) => {
+    return re.json();
+}).then((full) => { arr = full.data.memes});
+
+
+
+// console.log(console.log(arr));
+
+button.onclick = function () {
+    let num = Math.floor(Math.random() * arr.length);
+    img.src = arr[num].url;
+    
+}
